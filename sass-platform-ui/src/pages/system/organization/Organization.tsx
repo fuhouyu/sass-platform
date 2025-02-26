8345aa46999bfe9a78c1a63da570c6d6b43521ed
fuhouyu <fuhouyu@live.cn>
2025-02-26 21:30:32 +0800

    fix: i18n常量异常

--- sass-platform-ui/src/pages/system/organization/Organization.tsx
+++ sass-platform-ui/src/pages/system/organization/Organization.tsx
@@ -43,7 +43,7 @@ import './index.scss'
 import TextArea from "antd/es/input/TextArea";
 import {useOrganizationLazyData} from "@/hooks/useOrganizationLazyData.tsx";
 import {useLocaleStore} from "@/store";
-import {ZH_CN_LANGUAGE} from "@/constants/commonConstant.tsx";
+import {CommonConstant} from "@/constants/commonConstant.tsx";
 
 
 export const Organization = () => {
@@ -308,7 +308,7 @@ export const Organization = () => {
                     clearOnDestroy={true}
                     name="modal-form"
                     form={form}
-                    labelCol={{span: language == ZH_CN_LANGUAGE ? 4 : 8}}
+                    labelCol={{span: language == CommonConstant.ZH_CN_LANGUAGE ? 4 : 8}}
                     autoComplete="off"
                     initialValues={{
                         parentId: formParentOrganization.id,
